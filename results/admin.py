ba355713ad2fe73a7aded7a8e4de68dc223f83ec
Ринат Мухитов <malayka_73@inbox.ru>
2023-10-01 23:25:09 +0400

    Многоуровневое меню

--- results/admin.py
+++ results/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Results
+
+@admin.register(Results)
+class ResultsAdmin(admin.ModelAdmin):
+    list_display = ("sport","date_1")
