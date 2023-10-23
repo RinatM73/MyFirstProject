bf0a956091d0f17c92f7e9351e950d95b042a544
Ринат Мухитов <malayka_73@inbox.ru>
2023-10-23 23:37:16 +0400

    Главная страница. Начало

--- home/views.py
+++ home/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from news.models import News
+from video.models import Video
+
+
 def home(request):
-    return render(request, template_name='home/home.html')
+    news = News.objects.all()
+    videos = Video.objects.all()
+    return render(request, 'home/home.html', {'news_list': news, 'video_list': videos})
+
+
