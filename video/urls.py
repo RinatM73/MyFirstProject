da58498f66a785ac427de4ab89624e136ca4e772
Ринат Мухитов <malayka_73@inbox.ru>
2023-10-25 23:56:43 +0400

    Создал video_det.html

--- video/urls.py
+++ video/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('video/', views.videoview, name = 'video'),
+    path('<int:pk>', views.video_det, name = 'video_det'),
 ]
--- video/views.py
+++ video/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from video.models import Video
 
 def videoview(request):
     video = Video.objects.all()
     return render(request, 'video/video.html', {'video':video})
+
+def video_det(request, pk):
+    video = get_object_or_404(Video, id=pk)
+    return render(request, 'video_det/video_det.html', {'video_det': video})
